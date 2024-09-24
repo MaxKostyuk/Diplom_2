3b2f160ada4ad4107b5a1e6d03464a164441c489
kotan4ik <kostyuk.max@gmail.com>
2024-09-24 20:28:32 +0800

    feat: Gson replaced with Jackson; Jackson field added to BaseRequest ref: all dependency versions in pom.xml redefined in properties

--- src/test/java/com/kotan4ik/requests/BaseRequest.java
+++ src/test/java/com/kotan4ik/requests/BaseRequest.java
@@ -1,9 +1,19 @@
 package com.kotan4ik.requests;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
 
 public class BaseRequest {
+    private static ObjectMapper mapper;
+
     static {
         RestAssured.baseURI = "https://stellarburgers.nomoreparties.site/api/";
+        mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+    }
+
+    protected static <T> T fromJson(String jsonString, Class<T> classOfT) throws Exception {
+        return mapper.readValue(jsonString, classOfT);
     }
 }
