0a0d36c376487a2147e3178a810c73e90a446a6f
kotan4ik <kostyuk.max@gmail.com>
2024-09-25 12:28:13 +0800

    feat: new error message added to ErrorMessages

--- src/test/java/com/kotan4ik/ErrorMessages.java
+++ src/test/java/com/kotan4ik/ErrorMessages.java
@@ -2,4 +2,5 @@ package com.kotan4ik;
 
 public class ErrorMessages {
     public static final String USER_ALREADY_EXISTS = "User already exists";
+    public static final String NOT_ENOUGH_DATA = "Email, password and name are required fields";
 }
