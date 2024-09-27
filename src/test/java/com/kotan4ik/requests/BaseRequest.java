e94816df31f9813968bd0520e5ed4dd53bf269a4
kotan4ik <kostyuk.max@gmail.com>
2024-09-27 14:45:06 +0800

    feat: Jackson Java time support added and registered in BaseRequest

--- src/test/java/com/kotan4ik/requests/BaseRequest.java
+++ src/test/java/com/kotan4ik/requests/BaseRequest.java
@@ -2,6 +2,7 @@ package com.kotan4ik.requests;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.restassured.RestAssured;
 
 public class BaseRequest {
@@ -11,6 +12,7 @@ public class BaseRequest {
         RestAssured.baseURI = "https://stellarburgers.nomoreparties.site/api/";
         mapper = new ObjectMapper();
         mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
+        mapper.registerModule(new JavaTimeModule());
     }
 
     protected static <T> T fromJson(String jsonString, Class<T> classOfT) throws Exception {
